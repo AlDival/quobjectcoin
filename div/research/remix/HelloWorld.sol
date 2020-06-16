f03b0815d30b3e67d5b1cbb962b0e60df572624c
Matthias <matthias_ludwig@yahoo.com>
2020-06-16 16:21:38 +1000

    remix samples

---
+++ div/research/remix/HelloWorld.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+//pragma experimental ABIEncoderV2;
+
+contract HelloWorld {
+    string public message;
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+
+}
